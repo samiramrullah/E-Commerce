2a0fa9411fb94fea9f57ae62cecb1e5fb5797b33
samiramrullah <samiramrullah@gmail.com>
2022-10-31 02:43:42 +0530

    features

--- frontend/Components/hero.js
+++ frontend/Components/hero.js
@@ -1,5 +1,8 @@
 import React from 'react'
-
+import Image from 'next/image';
+import deliveryvechile from '../public/icons/delivery-van.svg';
+import moneyback from '../public/icons/money-back.svg';
+import supportimg from '../public/icons/service-hours.svg'
 const Hero = () => {
     return (
         <>
@@ -46,21 +49,22 @@ const Hero = () => {
             <div class="container py-16">
                 <div class="w-10/12 grid grid-cols-3 gap-6 mx-auto justify-center">
                     <div class="border border-primary rounded-sm px-3 py-6 flex justify-center items-center gap-5">
-                        <img src="assets/images/icons/delivery-van.svg" alt="Delivery" class="w-12 h-12 object-contain" />
+
+                        <Image class="w-12 h-12 object-contain" src={deliveryvechile}></Image>
                         <div>
                             <h4 class="font-medium capitalize text-lg">Free Shipping</h4>
                             <p class="text-gray-500 text-sm">Order over $200</p>
                         </div>
                     </div>
                     <div class="border border-primary rounded-sm px-3 py-6 flex justify-center items-center gap-5">
-                        <img src="assets/images/icons/money-back.svg" alt="Delivery" class="w-12 h-12 object-contain" />
+                        <Image class="w-12 h-12 object-contain" src={moneyback}></Image>
                         <div>
                             <h4 class="font-medium capitalize text-lg">Money Rturns</h4>
                             <p class="text-gray-500 text-sm">30 days money returs</p>
                         </div>
                     </div>
                     <div class="border border-primary rounded-sm px-3 py-6 flex justify-center items-center gap-5">
-                        <img src="assets/images/icons/service-hours.svg" alt="Delivery" class="w-12 h-12 object-contain" />
+                        <Image class="w-12 h-12 object-contain" src={supportimg}></Image>
                         <div>
                             <h4 class="font-medium capitalize text-lg">24/7 Support</h4>
                             <p class="text-gray-500 text-sm">Customer support</p>
