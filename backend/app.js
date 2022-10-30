712ce976d306425e8d1e666025caceb5b652b997
samiramrullah <samiramrullah@gmail.com>
2022-10-31 04:28:16 +0530

    body parser

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const morgan = require('morgan')
 const bodyParser = require('body-parser');
+
 const productRoutes = require('./api/routes/products')
 const app = express();
 
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use('/products', productRoutes)
 module.exports = app;
