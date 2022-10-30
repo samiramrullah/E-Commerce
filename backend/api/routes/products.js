41ae457f59729e49b344266f6857d6d07de1e849
samiramrullah <samiramrullah@gmail.com>
2022-10-31 04:18:41 +0530

    with basic product routes

--- backend/api/routes/products.js
+++ backend/api/routes/products.js
@@ -5,6 +5,25 @@ router.get('/', (req, res, next) => {
     res.status(200).json({
         message: 'I am product get'
     })
+});
+
+
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'I am product post'
+    })
+})
+
+router.patch('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'I am product patch'
+    })
+});
+
+router.delete('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'I am product delete'
+    })
 })
 
 module.exports = router;
