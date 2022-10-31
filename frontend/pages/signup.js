be3cd23b7b3c35e97221213add79f93595d993d9
samiramrullah <samiramrullah@gmail.com>
2022-10-31 17:18:32 +0530

    with navbar

--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link';
 import { useState } from 'react';
+import Navbar from '../Components/navbar'
 const Signup = () => {
     const [fName, setfName] = useState();
     const [email, setemail] = useState();
@@ -29,7 +30,7 @@ const Signup = () => {
     }
     return (
         <>
-
+            <Navbar />
             <div class="bg-grey-lighter min-h-screen flex flex-col">
                 <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
                     <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
