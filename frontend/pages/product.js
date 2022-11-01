547f1418e85e806278a7200428febe1ea5e8375b
samiramrullah <samiramrullah@gmail.com>
2022-11-01 20:26:39 +0530

    details link

--- frontend/pages/product.js
+++ frontend/pages/product.js
@@ -39,17 +39,8 @@ const Product = () => {
                     <div className=" grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 lg:gap-y-12 lg:gap-x-8 sm:gap-y-10 sm:gap-x-6 gap-y-6 lg:mt-12 mt-10">
 
                         {allProducts?.map(itm => (
-                            <Productcard title={itm.title} price={itm.price} img={itm.image} />
+                            <Productcard title={itm.title} price={itm.price} img={itm.image} id={itm.id} />
                         ))}
-                        {/* <Productcard />
-                        <Productcard />
-                        <Productcard />
-                        <Productcard />
-                        <Productcard />
-                        <Productcard />
-                        <Productcard />
-                        <Productcard />
-                        <Productcard /> */}
                     </div>
 
                     <div className=" flex justify-center items-center">
