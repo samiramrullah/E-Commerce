9a1fdebfbc0a2941c7b18fffc5c705823e86e8a9
samiramrullah <samiramrullah@gmail.com>
2022-11-01 13:47:00 +0530

    with error handling paths

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,8 @@ const morgan = require('morgan')
 const bodyParser = require('body-parser');
 const productRoutes = require('./api/routes/products');
 const userRouter = require('./api/routes/users');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const { application } = require('express');
 const app = express();
 
 
@@ -13,13 +14,14 @@ const db = 'mongodb://localhost:27017/E-Commerce';
 
 mongoose.connect(db).then(() => {
     console.log("Connection Successful");
-})
-    .catch((err) => console.log('Error'))
+}).catch((err) => console.log('Error'))
 mongoose.Promise = global.Promise;
+
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//handling cors error
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')  //wildcard
     res.header('Access-Control-Allow-Headers', '*');
@@ -30,6 +32,28 @@ app.use((req, res, next) => {
     next();
 })
 
+//Routes for handling request
 app.use('/products', productRoutes);
 app.use('/user', userRouter);
+
+//error handling if no path matched
+
+app.use((req, res, next) => {
+    const error = new Error('No matching paths')
+    error.status = 404;
+    next(error);
+})
+
+// if methods not matched
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
+
 module.exports = app;
