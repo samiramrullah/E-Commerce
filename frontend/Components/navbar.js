c042e369bea396ed4d97836b8f0b080fb3501a18
samiramrullah <samiramrullah@gmail.com>
2022-11-06 18:39:22 +0530

    cart count navbar

--- frontend/Components/navbar.js
+++ frontend/Components/navbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useState } from "react";
-
+import {useSelector} from 'react-redux';
 
 const Navbar = () => {
     const [searchInput, setSearchInput] = useState(true);
     const [mdOptionsToggle, setMdOptionsToggle] = useState(true);
     const [showMenu, setShowMenu] = useState(false);
+    const cartItems=useSelector((state)=>state.cart)
     return (
         <>
           <div className="dark:bg-gray-900">
@@ -104,11 +105,21 @@ const Navbar = () => {
                                         </svg>
                                     </button>
                                     <button aria-label="go to cart" className="text-gray-800 dark:hover:text-gray-300 dark:text-white focus:outline-none focus:ring-2 focus:ring-gray-800">
+                                        <div className='flex'>
+                                        <div>
                                         <svg className="fill-stroke" width={26} height={26} viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M5 1L1 5.8V22.6C1 23.2365 1.28095 23.847 1.78105 24.2971C2.28115 24.7471 2.95942 25 3.66667 25H22.3333C23.0406 25 23.7189 24.7471 24.219 24.2971C24.719 23.847 25 23.2365 25 22.6V5.8L21 1H5Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                                             <path d="M1 5.7998H25" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                                             <path d="M18.3346 10.6001C18.3346 11.8731 17.7727 13.094 16.7725 13.9942C15.7723 14.8944 14.4158 15.4001 13.0013 15.4001C11.5868 15.4001 10.2303 14.8944 9.23007 13.9942C8.22987 13.094 7.66797 11.8731 7.66797 10.6001" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+
                                         </svg>
+                                        </div>
+                                        <div className='pl-3'>
+                                          {cartItems.length}
+                                        </div>
+                                        
+                                        </div>
+                                        
                                     </button>
                                 </div>
                                 <div className="flex lg:hidden">
@@ -121,11 +132,13 @@ const Navbar = () => {
                                     </button>
                                     <button aria-label="open menu" onClick={() => setShowMenu(true)} className="text-black dark:text-white dark:hover:text-gray-300 md:hidden focus:outline-none focus:ring-2 rounded focus:ring-gray-600">
                                         <svg className="fill-stroke" width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            
                                             <path d="M4 6H20" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                                             <path d="M10 12H20" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                                             <path d="M6 18H20" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                                         </svg>
                                     </button>
+                            
                                 </div>
                             </div>
                         </div>
