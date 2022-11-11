f4f48612ef857c8179ec4fdc0a7fdae804634115
samiramrullah <samiramrullah@gmail.com>
2022-11-11 14:28:00 +0530

    redux cart added

--- frontend/Redux/CartSlice.js
+++ frontend/Redux/CartSlice.js
@@ -2,10 +2,31 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const CartSlice = createSlice({
     name: 'cart',
-    initialState:[],
+    initialState: {
+        items: [],
+        totalQuantity: 0,
+        totalPrice: 0,
+        totalpriceSum: 0,
+    },
     reducers: {
         addToCart(state, action) {
-            state.push(action.payload);
+            const newitem = action.payload;
+            const existingItem = state.items?.find((item) => item.id === newitem.id);
+            state.totalQuantity++;
+            if (!existingItem) {
+                state.items.push({
+                    id: newitem.id,
+                    price: newitem.price,
+                    quantity: 1,
+                    totalPrice: newitem.price,
+                    name: newitem.name,
+                });
+                
+            } else {
+                existingItem.quantity++;
+                existingItem.totalPrice =
+                    Number(existingItem.totalPrice) + Number(newitem.price);
+            }
         },
         removeFromCart(state, action) {
             state = state.filter((itm) => itm.id != action.payload)
@@ -15,3 +36,6 @@ const CartSlice = createSlice({
 
 export const { addToCart, removeFromCart } = CartSlice.actions;
 export default CartSlice.reducer;
+
+
+
